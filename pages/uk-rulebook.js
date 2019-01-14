853d81c86fbf00669cc12bf36d881e7928198a0e
Darko Stepanovic <dare.stepanovic@gmail.com>
2019-01-14 15:04:46 +0100

    Added UK Rulebook page

---
+++ pages/uk-rulebook.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Head from '../components/Head'
+import UkRulebook from '../components/UkRulebook'
+
+const Rulebook = () => {
+    return (
+        <>
+            <Head/>
+            <UkRulebook/>
+        </>
+    )
+};
+
+export default Rulebook;
