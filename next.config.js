15b6a97936b995921e87a1d83fbbe79363213994
Sergey Polonsky <polonsky.sergey@hotmail.com>
2019-01-25 23:57:37 +0100

    Changed default BASE_API_URL (apiv2 url) from dev into production url.

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   publicRuntimeConfig: {
       SELF_URL: process.env.SELF_URL || 'http://localhost:3000',
-      BASE_API_URL: process.env.BASE_API_URL || 'https://apiv2-dev.lykkex.net/api',
+      BASE_API_URL: process.env.BASE_API_URL || 'https://apiv2.lykke.com/api',
       WALLET_URL: process.env.WALLET_URL || 'https://wallet.lykke.com',
       TERMINAL_URL: process.env.TERMINAL_URL || 'https://trade.lykke.com',
       LYCI_ASSET_INDEX: process.env.LYCI_ASSET_INDEX || 'TLYCI',
