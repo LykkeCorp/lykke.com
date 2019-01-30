a29b9a594ee95511095364ade20150a41e6168ac
Darko Stepanovic <dare.stepanovic@gmail.com>
2019-01-30 13:47:41 +0100

    WEB-146 Re-Do Leadership page

--- pages/about/leadership.js
+++ pages/about/leadership.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Head from '../../components/Head'
 import Leadership from '../../components/Leadership'
+import Modal from "../../components/Leadership/Modal";
 
 export default () => {
     return (
