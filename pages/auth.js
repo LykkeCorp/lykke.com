9259070fe7cc204013874e895f8e5673e7f39c3d
Darko Stepanovic <dare.stepanovic@gmail.com>
2019-03-05 11:01:23 +0100

    Added clear storage on error

--- pages/auth.js
+++ pages/auth.js
@@ -10,7 +10,7 @@ class Auth extends Component {
                 window.location.origin + window.location.pathname);
             window.location.href = "/";
         }).catch(function (err) {
-            console.log('error: ', err);
+            sessionStorage.clear()
         });
     }
     render() {
