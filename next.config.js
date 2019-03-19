cad6de1ff1fe58dc153ac1962a43d080ccd22ec2
Andrey Zhylin <andrey.zhylin@outlook.com>
2019-03-19 14:37:30 +0300

    WALLET-43: Get auth urls from env vars

--- next.config.js
+++ next.config.js
@@ -15,6 +15,8 @@ module.exports = {
       LYCI_ASSET_INDEX: process.env.LYCI_ASSET_INDEX || 'TLYCI',
       P_LYCI_ASSET_INDEX: process.env.P_LYCI_ASSET_INDEX || '36eb1c74-11f0-4f1c-904f-d1ab42f69582',
       SC_LYCI_ASSET_INDEX: process.env.SC_LYCI_ASSET_INDEX || '02c6ce1e-e946-4c9a-a5ab-51b9314ba4ef',
-      LYCIUSD_ASSET_PAIR: process.env.LYCIUSD_ASSET_PAIR || 'TLYCIUSD'
+      LYCIUSD_ASSET_PAIR: process.env.LYCIUSD_ASSET_PAIR || 'TLYCIUSD',
+      AUTH_REDIRECT_URL: process.env.AUTH_REDIRECT_URL,
+      AUTH_SERVER_URL: process.env.AUTH_SERVER_URL
   }
 }
