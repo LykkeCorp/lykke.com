677c54438779e0f6b6582212384472ffb95ffd67
Ozgur GUL <lugrugzo@gmail.com>
2020-04-02 19:27:16 +0300

    WEB-301: Get min order amounts dynamically.

--- pages/api-wallet-trading-rules-fees-limits.js
+++ pages/api-wallet-trading-rules-fees-limits.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React from "react";
+import Head from "../components/Head";
+import { ApiFees } from "../components/ApiFees/ApiFees";
+import axios from "../axios";
 
-import Head from '../components/Head';
-import {ApiFees} from '../components/ApiFees/ApiFees';
+const ApiFeesPage = ({ minOrderAmounts }) => {
+  return (
+    <>
+      <Head
+        title="Lykke – Buy and sell cryptocurrency and digital assets"
+        description="Global marketplace for any kind of assets built on the top of blockchain technology"
+      />
+      <ApiFees minOrderAmounts={minOrderAmounts} />
+    </>
+  );
+};
 
+ApiFeesPage.getInitialProps = async function() {
+  const response = await axios.get("/assets/minOrderAmount");
+  const minOrderAmounts = (response.data || [])
+    .filter(a => a.AssetDisplayId)
+    .sort((a, b) => {
+      const textA = a.AssetDisplayId;
+      const textB = b.AssetDisplayId;
+      return textA < textB ? -1 : textA > textB ? 1 : 0;
+    });
+  return { minOrderAmounts };
+};
 
-const ApiFeesPage = (props) => {
-    return (
-        <>
-            <Head
-                title="Lykke – Buy and sell cryptocurrency and digital assets"
-                description="Global marketplace for any kind of assets built on the top of blockchain technology"
-            />
-            <ApiFees/>
-        </>
-    )
-}
-
-export default ApiFeesPage
+export default ApiFeesPage;
